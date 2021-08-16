8ea8aa0807da52d427a8e7e64b4fdd775274be2c
rueeeeeeng <qhaud7023@naver.com>
2021-08-16 21:30:17 +0900

    단순연결리스트 예제

--- src/chapter2/linkedList/SListTest.java
+++ src/chapter2/linkedList/SListTest.java
@@ -3,15 +3,15 @@ package chapter2.linkedList;
 public class SListTest {
 	public static void main(String[] args) {
 		SList fruits = new SList();
-		Node p = null;
+		Node p = new Node(fruits, null);
+		
 		fruits.insertFront("cherry"); // "pear" head 앞쪽에 추가
 		fruits.printList();
 
 		fruits.insertFront("pear"); // "pear" head 앞쪽에 추가
 		fruits.printList();
 
-		
-//		fruits.insertAfter(p.getNext(),"grape");
+		fruits.insertAfter("grape",p.getNext());
 		fruits.printList();
 
 
